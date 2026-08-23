ff8f1cc666fd10e492b62b38c335cc89a1e27e28
Vinícius de Paula Moraes <viniciusmoraes295651@gmail.com>
2026-08-23 13:33:27 -0300

    feat: create repository for store jokes

---
+++ src/fastapi_project/repository/jokes.py
@@ -0,0 +1,12 @@
+data: list[dict] = [
+    {
+        "setup": "Why do Python programmers prefer dark mode?",
+        "punchline": "Because light attracts bugs.",
+        "tag": "python",
+    },
+    {
+        "setup": "What's the object-oriented way to become wealthy?",
+        "punchline": "Inheritance.",
+        "tag": "python",
+    },
+]
--- src/fastapi_project/routes/jokes.py
+++ src/fastapi_project/routes/jokes.py
@@ -2,44 +2,32 @@ from random import choice
 
 from fastapi import APIRouter, HTTPException
 
+from fastapi_project.repository.jokes import data
 from fastapi_project.schemas.jokes import JokeCreate
 
 router = APIRouter(prefix="/jokes", tags=["Jokes"])
 
-jokes: list[dict] = [
-    {
-        "setup": "Why do Python programmers prefer dark mode?",
-        "punchline": "Because light attracts bugs.",
-        "tag": "python",
-    },
-    {
-        "setup": "What's the object-oriented way to become wealthy?",
-        "punchline": "Inheritance.",
-        "tag": "python",
-    },
-]
-
 
 @router.get("/")
-def get_joke(tag: str | None) -> dict:
+def get_joke(tag: str | None = None) -> dict:
     if tag is not None:
-        filtered: list[dict] = [j for j in jokes if j["tag"] == tag]
+        filtered: list[dict] = [j for j in data if j["tag"] == tag]
 
         if not filtered:
             raise HTTPException(status_code=404, detail=f"No jokes with tag '{tag}'")
         return choice(filtered)
-    return choice(jokes)
+    return choice(data)
 
 
 @router.get("/{joke_id}")
 def get_joke_by_id(joke_id: int) -> dict:
-    if joke_id < 0 or joke_id >= len(jokes):
+    if joke_id < 0 or joke_id >= len(data):
         raise HTTPException(status_code=404, detail=f"Joke not found with id {joke_id}")
-    return jokes[joke_id]
+    return data[joke_id]
 
 
 @router.post("/")
 def add_joke(joke: JokeCreate) -> dict:
     new_joke = {"setup": joke.setup, "punchline": joke.punchline, "tag": joke.tag}
-    jokes.append(new_joke)
-    return {"message": "Joke added!", "id": len(jokes) - 1, "joke": new_joke}
+    data.append(new_joke)
+    return {"message": "Joke added!", "id": len(data) - 1, "joke": new_joke}
