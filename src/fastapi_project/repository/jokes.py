e260d418f01a94b618302492a63db301ed4de20f
Vinícius de Paula Moraes <viniciusmoraes295651@gmail.com>
2026-08-23 14:21:19 -0300

    refactor: add id on jokes initial setup

--- src/fastapi_project/repository/jokes.py
+++ src/fastapi_project/repository/jokes.py
@@ -1,10 +1,12 @@
 data: list[dict] = [
     {
+        "id": 0,
         "setup": "Why do Python programmers prefer dark mode?",
         "punchline": "Because light attracts bugs.",
         "tag": "python",
     },
     {
+        "id": 1,
         "setup": "What's the object-oriented way to become wealthy?",
         "punchline": "Inheritance.",
         "tag": "python",
--- src/fastapi_project/routes/jokes.py
+++ src/fastapi_project/routes/jokes.py
@@ -3,12 +3,12 @@ from random import choice
 from fastapi import APIRouter, HTTPException
 
 from fastapi_project.repository.jokes import data
-from fastapi_project.schemas.jokes import JokeCreate
+from fastapi_project.schemas.jokes import JokeCreate, JokeRead
 
 router = APIRouter(prefix="/jokes", tags=["Jokes"])
 
 
-@router.get("/")
+@router.get("/", response_model=JokeRead)
 def get_joke(tag: str | None = None) -> dict:
     if tag is not None:
         filtered: list[dict] = [j for j in data if j["tag"] == tag]
@@ -19,7 +19,7 @@ def get_joke(tag: str | None = None) -> dict:
     return choice(data)
 
 
-@router.get("/{joke_id}")
+@router.get("/{joke_id}", response_model=JokeRead)
 def get_joke_by_id(joke_id: int) -> dict:
     if joke_id < 0 or joke_id >= len(data):
         raise HTTPException(status_code=404, detail=f"Joke not found with id {joke_id}")
@@ -28,6 +28,11 @@ def get_joke_by_id(joke_id: int) -> dict:
 
 @router.post("/")
 def add_joke(joke: JokeCreate) -> dict:
-    new_joke = {"setup": joke.setup, "punchline": joke.punchline, "tag": joke.tag}
+    new_joke = {
+        "id": len(data),
+        "setup": joke.setup,
+        "punchline": joke.punchline,
+        "tag": joke.tag,
+    }
     data.append(new_joke)
     return {"message": "Joke added!", "id": len(data) - 1, "joke": new_joke}
