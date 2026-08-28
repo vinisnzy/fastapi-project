5d52508bb2a139947cc83e65a11d04f770a2a2f7
Vinícius de Paula Moraes <viniciusmoraes295651@gmail.com>
2026-08-28 16:46:43 -0300

    feat: create sqlalchemy joke repository

--- src/fastapi_project/repository/jokes.py
+++ src/fastapi_project/repository/jokes.py
@@ -1,14 +1,53 @@
-data: list[dict] = [
-    {
-        "id": 0,
-        "setup": "Why do Python programmers prefer dark mode?",
-        "punchline": "Because light attracts bugs.",
-        "tag": "python",
-    },
-    {
-        "id": 1,
-        "setup": "What's the object-oriented way to become wealthy?",
-        "punchline": "Inheritance.",
-        "tag": "python",
-    },
-]
+from collections.abc import Sequence
+
+from sqlalchemy import select, update
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from fastapi_project.models import Joke
+from fastapi_project.repository.base_jokes import IJokeRepository
+from fastapi_project.schemas.jokes import JokeCreate, JokeRead, JokeUpdate
+
+
+class JokeRepository(IJokeRepository):
+    def __init__(self, session: AsyncSession) -> None:
+        self.session = session
+
+    async def get_all_jokes(self) -> Sequence[JokeRead]:
+        result = await self.session.execute(select(Joke))
+        return [JokeRead.model_validate(joke) for joke in result.scalars().all()]
+
+    async def get_jokes_by_tag(self, tag: str) -> Sequence[JokeRead]:
+        result = await self.session.execute(select(Joke).where(Joke.tag == tag))
+        return [JokeRead.model_validate(joke) for joke in result.scalars().all()]
+
+    async def get_joke_by_id(self, joke_id: str) -> JokeRead | None:
+        result = await self.session.execute(select(Joke).where(Joke.id == joke_id))
+        return JokeRead.model_validate(result.scalar_one_or_none())
+
+    async def add_joke(self, data: JokeCreate) -> JokeRead:
+        joke = Joke(**data.model_dump())
+        self.session.add(joke)
+        await self.session.commit()
+        return JokeRead.model_validate(joke)
+
+    async def update_joke(self, joke_id: str, data: JokeUpdate) -> JokeRead | None:        
+        setted_data = data.model_dump(exclude_unset=True)
+
+        if setted_data:
+            await self.session.execute(
+                update(Joke).where(Joke.id == joke_id).values(**setted_data)
+            )
+            await self.session.commit()
+
+        return await self.get_joke_by_id(joke_id)
+
+    async def delete_joke(self, joke_id: str) -> bool:
+        result = await self.session.execute(select(Joke).where(Joke.id == joke_id))
+        joke = result.scalar_one_or_none()
+
+        if not joke:
+            return False
+
+        await self.session.delete(joke)
+        await self.session.commit()
+        return True
