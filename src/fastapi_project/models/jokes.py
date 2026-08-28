60eae86bc156e316816c34873c1e81923742eefa
Vinícius de Paula Moraes <viniciusmoraes295651@gmail.com>
2026-08-27 21:56:51 -0300

    feat: create joke model

---
+++ src/fastapi_project/models/jokes.py
@@ -0,0 +1,12 @@
+from sqlalchemy import String
+from sqlalchemy.orm import Mapped, mapped_column
+
+from fastapi_project.models.base import Base, TimestampMixin, UUIDMixin
+
+
+class Joke(Base, UUIDMixin, TimestampMixin):
+    __tablename__ = "jokes"
+
+    setup: Mapped[str] = mapped_column(String(200), nullable=False)
+    punchline: Mapped[str] = mapped_column(String(100), nullable=False)
+    tag: Mapped[str] = mapped_column(String(50), nullable=False)
