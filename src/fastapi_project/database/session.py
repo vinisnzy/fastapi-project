2e0002b4bb88ee62df3f6be8686807b73760d098
Vinícius de Paula Moraes <viniciusmoraes295651@gmail.com>
2026-08-27 21:35:13 -0300

    feat: create sqlalchemy engine and async session and dependency

---
+++ src/fastapi_project/database/session.py
@@ -0,0 +1,23 @@
+from collections.abc import AsyncGenerator
+from typing import Annotated
+
+from fastapi import Depends
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
+
+from fastapi_project.core.config import settings
+
+engine = create_async_engine(url=settings.DATABASE_URL, echo=settings.DEBUG)
+
+async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
+
+
+async def get_async_session() -> AsyncGenerator[AsyncSession]:
+    async with async_session_maker() as session:
+        try:
+            yield session
+        except:
+            await session.rollback()
+            raise
+
+
+AsyncSessionDep = Annotated[AsyncSession, Depends(get_async_session)]
