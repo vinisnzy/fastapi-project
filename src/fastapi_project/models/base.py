7482f4e423d06e3d22499d235471a2cb4f320490
Vinícius de Paula Moraes <viniciusmoraes295651@gmail.com>
2026-08-27 21:56:13 -0300

    feat: create UUIDMixin model

--- src/fastapi_project/models/base.py
+++ src/fastapi_project/models/base.py
@@ -1,11 +1,16 @@
+import uuid
 from datetime import datetime
 
-from sqlalchemy import func
+from sqlalchemy import Uuid, func
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
 
 class Base(DeclarativeBase):
     """Base class for SQLAlchemy models"""
+
+
+class UUIDMixin:
+    id: Mapped[uuid.UUID] = mapped_column(Uuid, primary_key=True, default=uuid.uuid4)
 
 
 class TimestampMixin:
