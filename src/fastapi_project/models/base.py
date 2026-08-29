3560fe6d6ba2195b011ec0061b785ff7806b64db
Vinícius de Paula Moraes <viniciusmoraes295651@gmail.com>
2026-08-29 11:27:22 -0300

    feat: add naming convention on base model

--- src/fastapi_project/models/base.py
+++ src/fastapi_project/models/base.py
@@ -1,12 +1,22 @@
 import uuid
 from datetime import datetime
 
-from sqlalchemy import Uuid, func
+from sqlalchemy import MetaData, Uuid, func
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+
+NAMING_CONVENTION = {
+    "ix": "ix_%(column_0_label)s",
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(constraint_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s",
+}
 
 
 class Base(DeclarativeBase):
     """Base class for SQLAlchemy models"""
+
+    metadata = MetaData(naming_convention=NAMING_CONVENTION)
 
 
 class UUIDMixin:
