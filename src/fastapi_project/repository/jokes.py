6a2049fa714906e58377a1dd236771fa99a54dae
Vinícius de Paula Moraes <viniciusmoraes295651@gmail.com>
2026-08-30 12:16:22 -0300

    refactor: remove unused get repository function on joke repository

--- src/fastapi_project/repository/jokes.py
+++ src/fastapi_project/repository/jokes.py
@@ -80,7 +80,3 @@ class JokeRepository(IJokeRepository):
         await self.session.delete(joke)
         await self.session.commit()
         return True
-
-
-def get_joke_repository(session: AsyncSession):
-    return JokeRepository(session)
