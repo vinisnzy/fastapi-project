9b38367913404a6b87167dd7f773ab8429b1300e
Vinícius de Paula Moraes <viniciusmoraes295651@gmail.com>
2026-08-30 18:08:36 -0300

    refactor: create 'create_app' function

--- src/fastapi_project/main.py
+++ src/fastapi_project/main.py
@@ -4,11 +4,16 @@ from fastapi_pagination import add_pagination
 from fastapi_project.exceptions.error_handlers import register_error_handlers
 from fastapi_project.routers import jokes
 
-app = FastAPI()
 
-register_error_handlers(app)
-app.include_router(jokes.router)
-add_pagination(app)
+def create_app() -> FastAPI:
+    app = FastAPI()
+    register_error_handlers(app)
+    app.include_router(jokes.router)
+    add_pagination(app)
+    return app
+
+
+app = create_app()
 
 
 @app.get("/")
