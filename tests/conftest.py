2c3d394fbd5347c20a0ac5f90c19caa740dbd5bb
Vinícius de Paula Moraes <viniciusmoraes295651@gmail.com>
2026-08-31 07:20:39 -0300

    feat: add http config on conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,14 @@
 from collections.abc import AsyncIterator
+from unittest.mock import AsyncMock
 
 import pytest
+from fastapi import FastAPI
+from httpx import ASGITransport, AsyncClient
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from src.fastapi_project.dependencies import get_joke_service
+from src.fastapi_project.main import create_app
 from src.fastapi_project.models.base import Base
+from src.fastapi_project.services.jokes import JokeService
 
 
 @pytest.fixture(scope="session")
@@ -36,3 +42,25 @@ async def session(engine) -> AsyncIterator[AsyncSession]:
         await sess.close()
         await trans.rollback()
         await conn.close()
+
+
+@pytest.fixture
+def joke_service() -> AsyncMock:
+    return AsyncMock(spec=JokeService)
+
+
+@pytest.fixture
+async def app(joke_service: AsyncMock) -> AsyncIterator[FastAPI]:
+    app = create_app()
+    app.dependency_overrides[get_joke_service] = lambda: joke_service
+    yield app
+    app.dependency_overrides.clear()
+
+
+@pytest.fixture
+async def client(app: FastAPI) -> AsyncIterator[AsyncClient]:
+    async with AsyncClient(
+        transport=ASGITransport(app=app),
+        base_url="http://test",
+    ) as ac:
+        yield ac
