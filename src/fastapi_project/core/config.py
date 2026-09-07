e1f98b4fc7c6bc3d706707abfb5f401836547946
Vinícius de Paula Moraes <viniciusmoraes295651@gmail.com>
2026-09-06 21:53:32 -0300

    feat: add new env vars to auth on settings and to .env example

--- src/fastapi_project/core/config.py
+++ src/fastapi_project/core/config.py
@@ -9,6 +9,12 @@ BASE_DIR = Path(__file__).resolve().parents[3]
 class Settings(BaseSettings):
     DATABASE_URL: str
     DEBUG: bool = False
+
+    JWT_SECRET: str
+    JWT_ALGORITHM: str = "HS256"
+    ACCESS_TOKEN_EXPIRES_IN_MINUTES: int = 15
+    REFRESH_TOKEN_EXPIRES_IN_DAYS: 7
+
     model_config = SettingsConfigDict(
         env_file=BASE_DIR / ".env", env_file_encoding="utf-8", extra="ignore"
     )
