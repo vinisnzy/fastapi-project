a15911e41f995c11316d9e1b6d40b94a9c8dd6d4
Vinícius de Paula Moraes <viniciusmoraes295651@gmail.com>
2026-09-10 20:19:02 -0300

    chore: add auth router to app

--- src/fastapi_project/main.py
+++ src/fastapi_project/main.py
@@ -6,7 +6,7 @@ from fastapi_pagination import add_pagination
 from fastapi_project.core.config import Settings, get_settings
 from fastapi_project.database.session import build_engine, build_session_maker
 from fastapi_project.exceptions.error_handlers import register_error_handlers
-from fastapi_project.routers import jokes
+from fastapi_project.routers import auth, jokes
 
 
 def create_app(settings: Settings | None = None) -> FastAPI:
@@ -24,6 +24,7 @@ def create_app(settings: Settings | None = None) -> FastAPI:
     app = FastAPI(lifespan=lifespan)
     register_error_handlers(app)
     app.include_router(jokes.router)
+    app.include_router(auth.router)
     add_pagination(app)
 
     @app.get("/")
